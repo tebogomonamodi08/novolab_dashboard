99f4585445b7be8493d629298521a232cba0adf5
Tebogo <tebogomonamodi08@gmail.com>
2025-10-11 00:56:47 +0200

    email test script

---
+++ email_sender.py
@@ -0,0 +1,14 @@
+from smtplib import SMTP_SSL
+import ssl
+
+port = 465
+password = input('Enter password:')
+#create a ssl context
+context = ssl.create_default_context()
+with SMTP_SSL('smtp.gmail.com',port, context=context) as server:
+    server.login('tebogomonamodi08@gmail.com', password)
+    server.sendmail(
+        from_addr='tebogomonamodi08gmail.com',
+        to_addrs='tebogomonamodi64@gmail.com',
+        msg='subject: test mail\nHello There this is an automated email'
+    )
