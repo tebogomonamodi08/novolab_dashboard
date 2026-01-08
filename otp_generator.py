91823e39b5bc858c89f67799d66d0cc64322fd7c
Tebogo_Monamodi <150713484+tebogomonamodi08@users.noreply.github.com>
2026-01-08 13:23:42 +0200

    Remove unnecessary blank line in otp_generator.py

--- otp_generator.py
+++ otp_generator.py
@@ -1,6 +1,5 @@
 import secrets
 
-
 def generate_otp(length=6):
-    otp =  ''.join(str(secrets.randbelow(10)) for _ in range(length))
+    otp = ''.join(str(secrets.randbelow(10)) for _ in range(length))
     return otp
